9e0cf4a12b3d489c3410b1cb55c47810e745c01f
Jongmin Lee <miny@emagine.kr>
2014-06-09 18:28:28 +0900

    화면 터치하면 팽귄 발사

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by emagine on 2014. 6. 9..
+//  Copyright (c) 2014년 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
