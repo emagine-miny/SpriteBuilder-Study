0cf47f2dc2526b5965c315aef023d2e5739862d2
Jongmin Lee <miny@emagine.kr>
2014-06-09 17:07:39 +0900

    Create a Penguin sprite

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by emagine on 2014. 6. 9..
+//  Copyright (c) 2014년 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -0,0 +1,13 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by emagine on 2014. 6. 9..
+//  Copyright (c) 2014년 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+@end
