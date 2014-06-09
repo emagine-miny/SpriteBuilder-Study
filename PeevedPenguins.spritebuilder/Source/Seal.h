7d096c30573dc5d0d9cb38a4a9aaabc117273ba9
Jongmin Lee <miny@emagine.kr>
2014-06-09 17:08:50 +0900

    Create a Seal sprite

---
+++ PeevedPenguins.spritebuilder/Source/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by emagine on 2014. 6. 9..
+//  Copyright (c) 2014년 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,13 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by emagine on 2014. 6. 9..
+//  Copyright (c) 2014년 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+@end
